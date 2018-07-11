cd2a725b2a382af5c38d450219bab64f15584ba2
Kim YeongDeuk <panach3342@gmail.com>
2018-07-11 15:46:24 +0900

    선택정렬 사유 남김

--- 정렬/js.js
+++ 정렬/js.js
@@ -9,7 +9,8 @@ const arr_sort = {
     }, 
     mov_arr: (index, val, arr) => {
         console.log('mov_arr', index, val, arr)
-        arr
+        // 여기서부터 다시해야 한다
+        // 그래서 end_array 만들었다
     },
     map_move: (val, index, arr) => {
         let min_index = 0;
@@ -37,3 +38,11 @@ const arr_sort = {
 
 
 console.log(arr_sort.run(arr1));
+
+
+// 중도 포기
+//  남은 영역 
+// arr 을 인수로 주고 받고 있는대 마지막에 비교하여 자리를 교환한다해도 인수로 주고 받기에 교환해도 리셋된다
+// 그래서 run:  에서 받은 배열을  변수에 담는것이 어떤가 싶은대 전체 구조가 바뀔수도 있고
+// 지금으로서는 집중력이 0 이라 패스해야 겠다
+// 나중에 보자
